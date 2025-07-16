4b01ce1049d4a79d876d705b189f44f421b9a81d
emilyn0411 <lr.emaguad@mmdc.mcl.edu.ph>
2025-07-16 17:20:57 +0800

    Delete MotorPHEmployeeApp.java

--- MotorPHEmployeeApp.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.motorph;
-
-public class MotorPHEmployeeApp {
-    public static void main(String[] args) {
-        // Start your app here, for example:
-        LoginScreen.main(args);
-    }
-}
